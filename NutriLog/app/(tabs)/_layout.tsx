19094283bc237db929fdc8c4bc1c8fa69970f156
Andres Chavez <t4t4x777@gmail.com>
2025-04-25 00:20:45 -0400

    Navegador footer

--- NutriLog/app/(tabs)/_layout.tsx
+++ NutriLog/app/(tabs)/_layout.tsx
@@ -1,59 +1,47 @@
-import React from 'react';
+import { Ionicons } from '@expo/vector-icons';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link, Tabs } from 'expo-router';
-import { Pressable } from 'react-native';
-
-import Colors from '@/constants/Colors';
-import { useColorScheme } from '@/components/useColorScheme';
-import { useClientOnlyValue } from '@/components/useClientOnlyValue';
-
-// You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
-function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
-  color: string;
-}) {
-  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
-}
+import { Tabs } from 'expo-router';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        // Disable the static render of the header on web
-        // to prevent a hydration error in React Navigation v6.
-        headerShown: useClientOnlyValue(false, true),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerRight: () => (
-            <Link href="/modal" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name="info-circle"
-                    size={25}
-                    color={Colors[colorScheme ?? 'light'].text}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
-            </Link>
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="two"
-        options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-        }}
-      />
+      screenOptions={({ route }) => ({
+        tabBarActiveTintColor: '#fff',
+        tabBarInactiveTintColor: 'gray',
+        tabBarStyle: {
+          backgroundColor: '#244070',
+          height: 60,
+          // borderTopLeftRadius: 20,
+          // borderTopRightRadius: 20
+        },
+        tabBarIcon: ({ color, size }) => {
+          let iconName: keyof typeof Ionicons.glyphMap = 'home';
+          // indico los iconos
+          switch (route.name) {
+            case 'home':
+              iconName = 'home';
+              break;
+            case 'perfil':
+              iconName = 'person';
+              break;
+            case 'habito':
+              iconName = 'heart-sharp';
+              break;
+            case 'seguimiento':
+              iconName = 'bar-chart';
+              break;
+          }
+          return <Ionicons name={iconName} size={size} color={color} />
+        }
+      }
+      )
+      }>
+      
+      {/* indica que pantalla mostrar */}
+      <Tabs.Screen name="home" options={{ title: 'Menu' }} />
+      <Tabs.Screen name="seguimiento" options={{ title: 'Seguimiento' }} />
+      <Tabs.Screen name="habito" options={{ title: 'Habitos' }} />
+      <Tabs.Screen name="perfil" options={{ title: 'Perfil' }} />
     </Tabs>
   );
 }
